8c14b809b03e382f1b8c5bb77fca2f01bf44407f
Elian Gonzalez <elianngonzalez7@gmail.com>
2021-10-09 02:14:49 -0300

    refinando frontend & creando crud Personas

--- bdsqlit3/main_list.py
+++ bdsqlit3/main_list.py
@@ -1,11 +1,12 @@
 
 #-------------no terminado-----------------  
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
 #date time para hacer un timestamp
 #import datetime
 import pandas as pd
 import sqlite3
+from personas import *
+
 
 con = sqlite3.connect("DB\\vacunas")
 app= Flask(__name__)
@@ -14,230 +15,135 @@ app= Flask(__name__)
 app.secret_key = "mysecretkey"
 
 
-#-------------------------------FUNCIONES-----------------------------
+#-------------------------------FUNCIONES y RUTAS-----------------------------
 
 
-def Mostrar_Vacunas():
-    df2=pd.read_sql_query('SELECT nombre, dosis ,intervalo ,edad_min FROM vacunas',con)
-    print('LAS VACUNAS DISPONIBLES SON:')
-    print('++++++++++++++++++')
-    print(df2)
-    print('++++++++++++++++++')
+# routes
+@app.route('/')
+def deciciones():
+    return render_template('index.html')
+
+@app.route('/vacunas')
+def Vacunas():
+    con = sqlite3.connect("DB/vacunas")
+    data=con.execute('SELECT id, nombre, dosis,edad_min,estado_id,intervalo FROM vacunas')
+    est=con.execute('SELECT * FROM estado WHERE id')
+    return render_template('vacunas/index.html', vacunas = data, estado=est)
+
+@app.route('/add_vacuna', methods=['POST'])
+def add_vacuna():
+    if request.method == 'POST':
+        nombre = request.form['fullname']
+        dosis = request.form['dosis']
+        intervalo = request.form['intervalo']
+        edad_minima = request.form['edad_minima']
+        estado = request.form['estado']
+        con = sqlite3.connect("DB/vacunas")
+        con.execute("INSERT INTO vacunas (nombre, dosis ,intervalo,edad_min,estado_id) VALUES (?,?,?,?,?)", (nombre,dosis,intervalo,edad_minima,estado))
+        con.commit()
+        flash('Vacuna agregada correctamente')
+        return redirect(url_for('Vacunas'))
+
+@app.route('/edit/<id>', methods = ['POST', 'GET'])
+def get_vacuna(id):
+    con = sqlite3.connect("DB/vacunas")
+    cur=con.cursor() 
+    cur.execute('SELECT * FROM vacunas WHERE id = ?', (id))
+    data=cur.fetchall()
+    cur.execute('SELECT * FROM estado WHERE id')
+    est=cur.fetchall()
+    cur.close()
+    print(data)
+    return render_template('vacunas/edit-contact.html', vacuna = data[0], estados=est)
+
+@app.route('/update/<id>', methods=['POST'])
+def update_vacuna(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        dosis = request.form['dosis']
+        intervalo = request.form['intervalo'] 
+        edad = request.form['edad_minima']
+        estado = request.form['estado']   
+        con = sqlite3.connect("DB/vacunas")
+        cur = con.cursor()
+        cur.execute("UPDATE vacunas SET nombre = ?,dosis = ?,intervalo = ?, edad_min=? , estado_id=? WHERE id = ?", (fullname, dosis, intervalo,edad,estado, id))
+        con.commit()
+        flash('Vacuna editada correctamente')
+        return redirect(url_for('Vacunas'))
+
+@app.route('/delete/<string:id>', methods = ['POST','GET'])
+def delete_contact(id):
+    con = sqlite3.connect("DB/vacunas")
+    cur = con.cursor()
+    cur.execute('DELETE FROM vacunas WHERE id = {0}'.format(id))
+    con.commit()
+    flash('Vacuna Removida Correctamente')
+    return redirect(url_for('Vacunas'))
+
+#------------------PERSONAS----------------------
+
+@app.route('/personas')
+def IndexPersonas():
+    con = sqlite3.connect("DB/vacunas")
+    data=con.execute('SELECT id,nombre,apellido,dni,domicilio,fecha_nac,telefono FROM personas ')
+    return render_template('personas/index-personas.html', personas = data)
 
 
-def Buscar_vacunado():
-    x=input('ingrese el DNI: ')
-    datos=con.execute('SELECT nombre, dni, vacuna as vacuna_aplicada, dosis as numero_de_dosis, fecha_vacunacion FROM personas inner join registros_vac on persona.id=registros_vac.id inner join vacuna on vacuna.id=registros_vac.id where id =%s',(x))
-    #datos.fetchone['dni']
-    print('=' * 30)   
-    print(datos)    
-    print('=' * 30)
-    print()
+@app.route('/add_persona', methods = ['POST'])
+def add_Personas():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        dni = request.form['dni']
+        domicilio = request.form['domicilio']
+        fech_nac = request.form['fecha_d_nac']
+        telefono = request.form['telefono']
+        con = sqlite3.connect("DB/vacunas")
+        con.execute("INSERT INTO personas (nombre,apellido,dni,domicilio,fecha_nac,telefono) VALUES ", (nombre,apellido,dni,domicilio,fech_nac,telefono))
+        con.commit()
+        flash('Persona agregada correctamente')
+        return redirect(url_for('IndexPersonas'))
+
+@app.route('/edit_persona/<id>', methods = ['POST', 'GET'])
+def get_Personas(id):
+    con = sqlite3.connect("DB/vacunas")
+    cur=con.cursor() 
+    cur.execute('SELECT * FROM personas WHERE id = ?', (id))
+    data=cur.fetchall()
+    cur.close()
+    print(data)
+    return render_template('personas/edit-personas.html', vacuna = data[0])
 
 
-def Mostar_Personas():
-    dfpersonas = pd.read_sql_query('SELECT nombre, apellido,dni,domicilio, fecha_nac AS nacido_en FROM personas order by nombre', con)
-    print('LAS PERSONAS REGISTRADAS SON:')
-    print('*' * 30)
-    print()
-    print(dfpersonas.head())
-    print()
-    print('*' * 30)
+@app.route('/update_persona/<id>', methods = ['POST'])
+def update_Personas(id):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        dni = request.form['dni']
+        domicilio = request.form['domicilio']
+        fech_nac = request.form['fecha_d_nac']
+        telefono = request.form['telefono']   
+        con = sqlite3.connect("DB/vacunas")
+        cur = con.cursor()
+        cur.execute("UPDATE personas SET nombre=?,apellido=?,dni=?,domicilio=?,fecha_nac=?,telefono=? WHERE id = ?", (nombre,apellido,dni,domicilio,fech_nac,telefono, id))
+        con.commit()
+        flash('Vacuna editada correctamente')
+        return redirect(url_for('IndexPersonas'))
 
 
-def Mostrar_Registro_1a_dosis():
-    datos=con.execute('SELECT nombre, dni, vacuna as vacuna_aplicada, dosis as numero_de_dosis, fecha_vacunacion FROM personas inner join registros_vac on persona.id=registros_vac.id inner join vacuna on vacuna.id=registros_vac.id where dosis = 1')
-    print('=' * 30)
-    print(datos)
-    print('=' * 30)
-
-def Mostrar_Registro_2a_dosis():
-    datos=con.execute('SELECT nombre, dni, vacuna as vacuna_aplicada, dosis as numero_de_dosis, fecha_vacunacion FROM personas inner join registros_vac on persona.id=registros_vac.id inner join vacuna on vacuna.id=registros_vac.id where dosis = 2')
-    print('=' * 30)
-    print(datos)
-    print('=' * 30)
-
-#esto se hace con un formulario de html
-def Crear_Persona():
-    new_nombre=input('ingrese el nuevo nombre:')
-    new_apellido=input('ingrese el nuevo apellido: ')
-    new_dni=input('ingrese el nuevo D.N.I: ')
-    new_direccion=input('ingrse la nueva direccion: ')
-    #no completo
-    insert=pd.read_sql_query('INSERT INTO personas ( id,nombre,apellido,dni,domicilio,fecha_nac,telefono,fecha_creacion) VALUES (NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);')
-    
-    
-    #personas.append([new_nombre,new_apellido,new_dni,new_direccion])
-
-
-def Mostrar_Registro():
-    print('TODOS LOS REGISTRADOS SON:')
-    for registrado in registro:
-        print('=' * 30)
-        print(f'Apellido:{registrado[0][1]} \n Nombre:{registrado[0][0]} \n D.N.I:{registrado[0][2]} \n Vacuna aplicada:{registrado[1][0]} \n N° de dosis:{registrado[2]} \n fecha de inmunizacion:{registrado[3]} ')
-        print('=' * 30)
-
-
-def Eliminar_Persona():
-    Mostar_Personas()
-    Rta=input('ingrese el DNI a eliminar: ')
-    for pers in personas:
-        if Rta==pers[2]:
-            print(f'Se elimino a {Rta}')
-            personas.pop(personas.index(pers))
-        else:
-            print(f'no se encontro a {Rta} ')
-
-def Editar_Persona():
-    Mostar_Personas()
-    person = input('ingrese el DNI de la persona:')
-    for registrado in personas:
-        if person == registrado[2]:
-            print('¿QUE DATO DESEA CAMBIAR?')
-            print()
-            print('1 --- NOMBRE ')
-            print('2 --- APELLIDO ')
-            print('3 --- D.N.I ')
-            print('4 --- DIRECCION ')
-            Rta=input('ingrese el N°: ')
-            if Rta == '1':
-                registrado[0] = input('ingrese el nombre: ')
-            elif Rta == '2':
-                registrado[1] = input('ingrese el apellido:')
-            elif Rta == '3':
-                registrado[2] = input('ingrese el D.N.I: ')
-            elif Rta == '4':
-                registrado[3] = input('ingrse la direccion')
-            else:
-                print(f'no se encontro a la persona {person} ')
-
-
-
-
-
-
-def Crear_Registro_New():
-    print('seleccione el DNI de la persona a vacunar')
-    Mostar_Personas()
-    persoN=input('ingrese el DNI: ')
-    for pers in personas:
-        if persoN==pers[2]:
-            print(f'Se agrega a {pers[0][0]}')
-    print('|'*30)
-    print('ingrese el N° de vacuna a colocar')
-    Mostrar_Vacunas()
-    vacN = int(input('ingrese el N°: '))-1
-    for vacu in vacunas:
-        if  vacN-1 == vacunas.index(vacu):
-            print(f'Se vacuna con: {vacu[0]}')
-            dosisN = int(input('N° de dosis: '))
-            fecha_dosis = input('Fecha del dia:')
-            registro.append([pers, vacu, dosisN, fecha_dosis])
-        elif vacN-1 !=  vacunas.index(vacu) :
-            print('no puede colocarse una vacuna diferente')
-
-
-
-
-
-''' ----FIJATE EL EJERCICIO DE ELIMINAR POR INDEX DE LA CLACE DE MARCELO,HACE QUE SOLO SE LECCIONE EL INDEX
-    Y LO COLOCAS CON APPEND([]) EN REGISTRO...
-    CORDATE QUE TENES QUE HACER EDITABLE LOS DATOS DE LAS PERSONAS(ESO TAMBIEN ESTA EN LA CLASE DE MARCELO)
-    -----'''
+@app.route('/delete_persona/<string:id>', methods = ['POST', 'GET'])
+def delete_Personas(id):
+    con = sqlite3.connect("DB/vacunas")
+    cur = con.cursor()
+    cur.execute('DELETE FROM personas WHERE id = {0}'.format(id))
+    con.commit()
+    flash('Persona Removida Correctamente')
+    return redirect(url_for('IndexPersonas'))
 
 
 
-'''------------------------DATOS--------------------------------'''
+# starting the app
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
 
-'''vacunas=[
-    ['sputnik V',2,21,18],
-    ['Covishield',2,56,18],
-    ['Sinopharm', 2,21,18],
-    ['AstraZeneca', 2, 56,18]
-]
-personas=[
-    ['Ignacio','Gomez','21323232','Madariaga 1900'],
-    ['Maria','Palma','35465454','Rivadavia 1225'],
-    ['Miguel', 'Gonzalez', '43545455', 'Colon s/n']
-]
-
-registro=[
-    [personas[1],vacunas[0],1,'12/06/2021'],
-    [personas[0],vacunas[2],1,'01/05/2021'],
-    [personas[2],vacunas[1],1,'12/05/2021'],
-    [personas[1],vacunas[0],2,'20/06/2021']
-]'''
-
-#-------------------INICIO DEL PROGRAMA------------------------
-print('-------------------REGISTROS COVID_19--------------------')
-print()
-while True :
-
-    print(' 1 --- ver los tipos de vacunas disponibles')
-    print(' 2 --- ver datos de personas')
-    print(' 3 --- eliminar persona ')
-    print(' 4 --- cambiar datos de una persona ')
-    print(' 5 --- ingresar una nueva persona')
-    print(' 6 --- ver personas vacunadas con primera dosis')
-    print(' 7 --- ver personas vacunadas con segunda dosis')
-    print(' 8 --- ver el listado de vacunados completo')
-    print(' 9 --- buscar vacunado ')
-    print(' 10 --- registrar vacunado ')
-    print()
-    print(' 11 --- salir ')
-    print('-'*30)
-
-    respuesta = input('ingrese el número de accion que desea:')
-    if respuesta == '1':
-        Mostrar_Vacunas()
-    elif respuesta == '2':
-        Mostar_Personas()
-    else:
-        print('MUCHAS GRACIAS!!!')
-        break
-    '''
-    elif respuesta == '3':
-        Eliminar_Persona()
-    elif respuesta == '4':
-        Editar_Persona()
-    elif respuesta == '5':
-        Crear_Persona()
-    elif respuesta == '6':
-        Mostrar_Registro_1a_dosis()
-    elif respuesta == '7':
-        Mostrar_Registro_2a_dosis()
-    elif respuesta == '8':
-        Mostrar_Registro()
-    elif respuesta == '9':
-        Buscar_vacunado()
-    elif respuesta == '10':
-        Crear_Registro_New()
-'''
-con.close()
-
-
-#------------------------------FIN, CREO--------------------------------
-
-
-
-
-'''bdef Crear_Registro_New():
-    print('seleccione el DNI de la persona a vacunar')
-    Mostar_Personas()
-    persoN=input('ingrese el DNI: ')
-    for pers in personas:
-        if persoN==pers[2]:
-            print(f'Se agrega a {pers[0]} {pers[1]}')
-    print('|'*30)
-    print('ingrese el N° de vacuna a colocar')
-    Mostrar_Vacunas()
-    vacN = int(input('ingrese el N°: '))-1
-    for vacu in vacunas:
-        if vacN == vacunas.index(vacu):
-            print(f'Se vacuna con: {vacu[0]}')
-
-    dosisN=int(input('N° de dosis: '))
-    fecha_dosis=input('Fecha del dia:')
-    registro.append([pers,vacu,dosisN,fecha_dosis])
-
-    '''
